ea4c8ef83f323d42a270583526c04e04906bb8c9
Jan Cerveny <cervenyjan@yahoo.co.uk>
2019-10-07 22:14:09 +0200

    Adding a sample test to test UnitTestins

---
+++ Com.WIC.Tests.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Com.WIC.Tests.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
