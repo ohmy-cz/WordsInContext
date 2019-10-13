f6fe5a4715c4fc9b2db34f966afafea72f7d150e
Jan Cerveny <cervenyjan@yahoo.co.uk>
2019-10-13 14:45:02 +0200

    Adding a watch task

--- Application/Com.WIC.Client.Web/gulpfile.js
+++ Application/Com.WIC.Client.Web/gulpfile.js
@@ -1,29 +1,17 @@
-﻿const { src, dest, parallel } = require('gulp');
-//const pug = require('gulp-pug');
+﻿const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
-//const concat = require('gulp-concat');
-
-//function html() {
-//    return src('client/templates/*.pug')
-//        .pipe(pug())
-//        .pipe(dest('build/html'))
-//}
 
 function css() {
     return src('SCSS/*.scss', { sourcemaps: true })
         .pipe(sass())
         .pipe(minifyCSS())
-        .pipe(dest('wwwroot/css'))
+        .pipe(dest('wwwroot/css'));
 }
 
-//function js() {
-//    return src('client/javascript/*.js', { sourcemaps: true })
-//        .pipe(concat('app.min.js'))
-//        .pipe(dest('build/js', { sourcemaps: true }))
-//}
+function watchCSS() {
+    watch('SCSS/*.scss', css);
+}
 
-//exports.js = js;
-//exports.css = css;
-//exports.html = html;
-exports.default = /*parallel(html, */css/*, js)*/;
+exports.watch = watchCSS;
+exports.default = css;
